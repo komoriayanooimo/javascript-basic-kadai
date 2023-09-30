5f287300a31ccdb07997c867928b28cf7d6c44c4
komoriayanooimo <ayano.ak7@icloud.com>
2023-10-01 00:57:58 +0900

    課題5

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言・値の代入
+let bodyWeight = '体重';
+
+// 変数の宣言・値の代入
+let height = '身長';
+
+// 変数の宣言・値の代入
+let bmi = '体格指数';
+
+// 四則演算
+console.log(68/(1.7*1.7));
+
+// コンソールへの出力
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
