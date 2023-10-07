cd942132223403c586c2dc7dfdb3b16dd576c336
komoriayanooimo <ayano.ak7@icloud.com>
2023-10-07 10:50:03 +0900

    課題20訂正

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+const text = document.getElementById('text');
+
+function textContent(){
+    document.getElementById('text').
+    setTimeout((function){
+        textContent ='ボタンをクリックしました';
+  }
+, 2000);
+  
+  document.getElementById ('btn').addEventListener('click'.textContent)
+  
+}
