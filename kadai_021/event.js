2fe1fb58118e9b6072dad71ef7fc83bd0c97c190
komoriayanooimo <ayano.ak7@icloud.com>
2023-10-09 17:04:10 +0900

    課題021

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,6 +1,8 @@
-function changeText() {
-  setTimeout(() => {
+const btn = document.getElementById('btn');
+btn.addEventListener('click',() => {
+ 
+setTimeout(() => {
     const text = document.getElementById('text');
     text.textContent = "ボタンをクリックしました";
   }, 2000);
-}
+});
